406347ea2ce7aa43f8dd4bc57d4e30fb81515442
Jason Ozias <jason.g.ozias@gmail.com>
2014-03-19 15:14:33 -0400

    Fixed some issues with executeQuery and added more robust tests

--- test/test-hsqldb.js
+++ test/test-hsqldb.js
@@ -1,32 +1,76 @@
 var nodeunit = require('nodeunit');
-var hsqldbConn = require('../lib/jdbc.js');
+var jdbcConn = require('../lib/jdbc.js');
 
 var config = {
-  libpath: __dirname + '/hsqldb.jar',
+  libpath: './drivers/hsqldb.jar',
   drivername: 'org.hsqldb.jdbc.JDBCDriver',
-  url: 'jdbc:hsqldb:mem:testdb;user=SA;password='
+  url: 'jdbc:hsqldb:hsql://localhost/xdb;user=SA;password='
 };
 
-module.exports['hsqldb'] = nodeunit.testCase({
-  "test initialize": function(test) {  
-    hsqldbConn.on('init', function(drivername) {
+module.exports = {
+  tearDown: function(callback) {
+    jdbcConn.removeAllListeners();
+    callback();
+  },
+  testinit: function(test) {  
+    jdbcConn.on('init', function(drivername) {
+      test.expect(1);
       test.equal(drivername, 'org.hsqldb.jdbc.JDBCDriver');
       test.done();
     });
-    hsqldbConn.initialize(config);   
+    jdbcConn.initialize(config);   
   },
-  "test open": function(test) {
-    hsqldbConn.on('open', function(conn) {
+  testopen: function(test) {
+    jdbcConn.on('open', function(conn) {
+      test.expect(1);
       test.ok(conn);
       test.done();
     });
-    hsqldbConn.open();
+    jdbcConn.open();
   },
-  "test close": function(test) {
-    hsqldbConn.on('close', function(status) {
-      test.equal('closed',status);
+  testcreatetable: function(test) {
+    jdbcConn.on('executeQuery', function(err, result) {
+      test.expect(2);
+      test.equal(null, err);
+      test.ok(result);
       test.done();
     });
-    hsqldbConn.close();
-  }
-});
+    jdbcConn.executeQuery("CREATE TABLE blah (id int, name varchar(10));");
+  },
+  testinsert: function(test) {
+    jdbcConn.on('executeQuery', function(err, result) {
+      test.expect(2);
+      test.equal(null, err);
+      test.ok(result);
+      test.done();
+    });
+    jdbcConn.executeQuery("INSERT INTO blah VALUES (1, 'Jason');");
+  },
+  testselect: function(test) {
+    jdbcConn.on('executeQuery', function(err, result) {
+      test.expect(2);
+      test.equal(null, err);
+      test.ok(result && result.length == 1);
+      test.done();
+    });
+    jdbcConn.executeQuery("SELECT * FROM blah;");
+  },
+  testdroptable: function(test) {
+    jdbcConn.on('executeQuery', function(err, result) {
+      test.expect(2);
+      test.equal(null, err);
+      test.ok(result);
+      test.done();
+    });
+    jdbcConn.executeQuery("DROP TABLE blah;");
+  },
+  testshutdown: function(test) {
+    jdbcConn.on('executeQuery', function(err, result) {
+      test.expect(2);
+      test.equal(null, err);
+      test.ok(result);
+      test.done();
+    });
+    jdbcConn.executeQuery("SHUTDOWN");
+  },
+};
