226f38f3063c92f1d5490d200652ac8294eccdaa
group6 <cjhin@civisanalytics.com>
2014-04-12 19:09:56 -0400

    update tests to reflect callbacks

--- test/test-hsqldb.js
+++ test/test-hsqldb.js
@@ -9,115 +9,102 @@ var config = {
 
 module.exports = {
   tearDown: function(callback) {
-    jdbcConn.removeAllListeners();
     callback();
   },
   testinit: function(test) {  
-    jdbcConn.on('init', function(err, drivername) {
+    jdbcConn.initialize(config, function(err) {
       test.expect(2);
       test.equal(null, err)
       test.equal(drivername, 'org.hsqldb.jdbc.JDBCDriver');
       test.done();
     });
-    jdbcConn.initialize(config);   
   },
   testopen: function(test) {
-    jdbcConn.on('open', function(err, conn) {
+    jdbcConn.open(function(err, conn) {
       test.expect(2);
       test.equal(null, err);
       test.ok(conn);
       test.done();
     });
-    jdbcConn.open();
   },
   testcreatetable: function(test) {
-    jdbcConn.on('executeQuery', function(err, result) {
+    jdbcConn.executeQuery("CREATE TABLE blah (id int, name varchar(10));", function(err, result) {
       test.expect(2);
       test.equal(null, err);
       test.ok(result);
       test.done();
     });
-    jdbcConn.executeQuery("CREATE TABLE blah (id int, name varchar(10));");
   },
   testeqinsert: function(test) {
-    jdbcConn.on('executeQuery', function(err, result) {
+    jdbcConn.executeQuery("INSERT INTO blah VALUES (1, 'Jason');", function(err, result) {
       test.expect(2);
       test.equal(null, err);
       test.ok(result);
       test.done();
     });
-    jdbcConn.executeQuery("INSERT INTO blah VALUES (1, 'Jason');");
   },
   testeuinsert: function(test) {
-    jdbcConn.on('executeUpdate', function(err, result) {
+    jdbcConn.executeUpdate("INSERT INTO blah VALUES (3, 'Temp');", function(err, result) {
       test.expect(2);
       test.equal(null, err);
       test.ok(result && result == 1);
       test.done();
     });
-    jdbcConn.executeUpdate("INSERT INTO blah VALUES (3, 'Temp');");
   },
   testequpdate: function(test) {
-    jdbcConn.on('executeQuery', function(err, result) {
+    jdbcConn.executeQuery("UPDATE blah SET id = 2 WHERE name = 'Jason';", function(err, result) {
       test.expect(2);
       test.equal(null, err);
       test.ok(result);
       test.done();
     });
-    jdbcConn.executeQuery("UPDATE blah SET id = 2 WHERE name = 'Jason';");
   },
   testeuupdate: function(test) {
-    jdbcConn.on('executeUpdate', function(err, result) {
+    jdbcConn.executeUpdate("UPDATE blah SET id = 4 WHERE name = 'Temp';", function(err, result) {
       test.expect(2);
       test.equal(null, err);
       test.ok(result && result == 1);
       test.done();
     });
-    jdbcConn.executeUpdate("UPDATE blah SET id = 4 WHERE name = 'Temp';");
   },
   testselect: function(test) {
-    jdbcConn.on('executeQuery', function(err, result) {
+    jdbcConn.executeQuery("SELECT * FROM blah;", function(err, result) {
       test.expect(2);
       test.equal(null, err);
       test.ok(result && result.length == 2);
       test.done();
     });
-    jdbcConn.executeQuery("SELECT * FROM blah;");
   },
   testeqdelete: function(test) {
-    jdbcConn.on('executeQuery', function(err, result) {
+    jdbcConn.executeQuery("DELETE FROM blah WHERE id = 2;", function(err, result) {
       test.expect(2);
       test.equal(null, err);
       test.ok(result);
       test.done();
     });
-    jdbcConn.executeQuery("DELETE FROM blah WHERE id = 2;");
   },
   testeudelete: function(test) {
-    jdbcConn.on('executeUpdate', function(err, result) {
+    jdbcConn.executeUpdate("DELETE FROM blah WHERE id = 4;", function(err, result) {
       test.expect(2);
       test.equal(null, err);
       test.ok(result && result == 1);
       test.done();
     });
-    jdbcConn.executeUpdate("DELETE FROM blah WHERE id = 4;");
   },
   testdroptable: function(test) {
-    jdbcConn.on('executeQuery', function(err, result) {
+    jdbcConn.executeQuery("DROP TABLE blah;", function(err, result) {
       test.expect(2);
       test.equal(null, err);
       test.ok(result);
       test.done();
     });
-    jdbcConn.executeQuery("DROP TABLE blah;");
   },
   testshutdown: function(test) {
-    jdbcConn.on('executeQuery', function(err, result) {
+    jdbcConn.executeQuery("SHUTDOWN", function(err, result) {
       test.expect(2);
       test.equal(null, err);
       test.ok(result);
       test.done();
     });
-    jdbcConn.executeQuery("SHUTDOWN");
   },
 };
