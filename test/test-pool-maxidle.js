58f47f9cd6f163922b3fa8ea1e3af46fdb819862
Mario Lyon <a-mlyon@expedia.com>
2016-04-14 14:39:09 +0100

    Maxidle feature only kicks in if keepalive is disable, and maxidle value is passed in the config

--- test/test-pool-maxidle.js
+++ test/test-pool-maxidle.js
@@ -35,25 +35,36 @@ var configWithMaxIdle = {
   maxidle: 20*60*1000 //20 minutes
 };
 
+var configWithMaxIdleAndKeepAlive = {
+  url: 'jdbc:hsqldb:hsql://localhost/xdb',
+  user : 'SA',
+  password: '',
+  minpoolsize: 1,
+  maxpoolsize: 1,
+  maxidle: 20*60*1000,
+  keepalive: {
+    interval: 45*60*1000,
+    query: 'select 1',
+    enabled: true
+  }
+};
+
+
 var testpool = null;
 var conn1Uuid = null;
 var clock = null;
 module.exports = {
   group1: {
     setUp: function(callback){
-    if (testpool === null) {
-        clock = lolex.install();
-        testpool = new Pool(config);
+      clock = lolex.install();
+      testpool = new Pool(config);
 
-        return Q.ninvoke(testpool, 'reserve')
-            .then(function (conn) {
-              conn1Uuid = conn.uuid;
-              return Q.ninvoke(testpool, 'release', conn);
-            })
-            .then(callback)
-        callback();
-      }
-      callback();
+      return Q.ninvoke(testpool, 'reserve')
+          .then(function (conn) {
+            conn1Uuid = conn.uuid;
+            return Q.ninvoke(testpool, 'release', conn);
+          })
+          .then(callback);
     },
     tearDown: function(callback) {
       clock.uninstall();
@@ -67,6 +78,7 @@ module.exports = {
             console.log(err);
           } else {
             test.expect(4);
+            //expect the same connection to be returned
             test.equal(conn1Uuid, conn.uuid);
             test.equal(null, err);
             test.equal(testpool._pool.length, 0);
@@ -79,21 +91,17 @@ module.exports = {
   group2: {
     setUp: function(callback) {
       clock = lolex.install();
+      testpool = new Pool(configWithMaxIdle);
 
-      if (testpool === null) {
-        testpool = new Pool(configWithMaxIdle);
-
-        return Q.ninvoke(testpool, 'reserve')
-            .then(function(conn) {
-              conn1Uuid = conn.uuid;
-              return Q.ninvoke(testpool, 'release', conn);
-            })
-            .then(callback)
-            .catch(function(e){
-              console.log(e);
-            });
-      }
-      callback();
+      return Q.ninvoke(testpool, 'reserve')
+          .then(function(conn) {
+            conn1Uuid = conn.uuid;
+            return Q.ninvoke(testpool, 'release', conn);
+          })
+          .then(callback)
+          .catch(function(e){
+            console.log(e);
+          });
     },
     tearDown: function(callback) {
       clock.uninstall();
@@ -106,7 +114,43 @@ module.exports = {
           console.log(err);
         } else {
           test.expect(4);
+          //expect a new connection
           test.notEqual(conn1Uuid, conn.uuid);
+          test.equal(null, err);
+          test.equal(testpool._pool.length, 0);
+          test.equal(testpool._reserved.length, 1);
+          test.done();
+        }
+      });
+    }
+  },
+  group3: {
+    setUp: function(callback) {
+      clock = lolex.install();
+
+      testpool = new Pool(configWithMaxIdleAndKeepAlive);
+
+      return Q.ninvoke(testpool, 'reserve')
+        .then(function(conn) {
+          conn1Uuid = conn.uuid;
+          return Q.ninvoke(testpool, 'release', conn);
+        })
+        .then(callback)
+        .catch(callback);
+    },
+    tearDown: function(callback) {
+      clock.uninstall();
+      callback();
+    },
+    testreserve_after_max_idle_time_with_keepalive: function(test) {
+      clock.tick("40:00");
+      testpool.reserve(function(err, conn) {
+        if (err) {
+          console.log(err);
+        } else {
+          test.expect(4);
+          //we expect the same connection to be retrieved
+          test.equals(conn1Uuid, conn.uuid);
           test.equal(null, err);
           test.equal(testpool._pool.length, 0);
           test.equal(testpool._reserved.length, 1);
